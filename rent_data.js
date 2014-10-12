fc00b23cc9f58e479feaefede17a5813ae9769a9
Fredrick <fredrickprashanth@gmail.com>
2014-10-11 23:36:01 -0700

    Change to plain d3.js barcharts

--- rent_data.js
+++ rent_data.js
@@ -2,7 +2,13 @@ redis_rent_data = require("./rent_data_redis");
 
 exports.get_rent_chart_data = function(req, res) {
   redis_rent_data.get_city_mean_rent(req.params.city, function(err, data) {
-    res.json(data);
+    var rent_data = [];
+    data.forEach(function(rent_rec, i, arr) {
+      var count = +rent_rec.count;
+      if (count)
+        rent_data.push(rent_rec);
+    });
+    res.json(rent_data);
   }); 
 }
 
--- rent_data_redis.js
+++ rent_data_redis.js
@@ -13,22 +13,18 @@ var get_city_mean_rent_bd = function(city, bd, mean_rent_cb) {
 
 exports.get_city_mean_rent = function(city, mean_rent_cb) {
   var rent_data = new Array();
-  var nr_done = 0;
   var bds = new Array(1, 2, 3, 4);
-  for (var bd = 0; bd < 5; bd++) {
-    bds.forEach(function(bd, i, arr) {
-      get_city_mean_rent_bd(city, bd, function(err, bd, mean_rent, count) {
-        if (err) {
-          mean_rent_cb(err, rent_data);  
-        } else {
-          rent_data.push([mean_rent, count]);
-          if (++nr_done == 4) {
-            mean_rent_cb(err, rent_data);
-          }
-        }
-      }); // get_city_mean_rent_bd
-    }); // forEach
-  } // for
+  bds.forEach(function(bd, i, arr) {
+    get_city_mean_rent_bd(city, bd, function(err, bd, mean_rent, count) {
+      if (err) {
+        mean_rent_cb(err, rent_data);  
+      } else {
+        rent_data.push({bd:bd, rent:mean_rent, count:count});
+        if (rent_data.length == bds.length)
+          mean_rent_cb(err, rent_data);
+      }
+    }); // get_city_mean_rent_bd
+  }); // forEach
 } // get_city_mean_rent
 
 exports.get_cities = function(cities_cb) {
